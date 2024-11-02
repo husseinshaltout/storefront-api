318228b379df2f8fa1ae1bf745a35f3aab254525
Hussein Shaltout <48390004+husseinshaltout@users.noreply.github.com>
2024-11-02 22:13:55 +0000

    refactor(user): use dbloader

--- src/modules/user/user.repository.ts
+++ src/modules/user/user.repository.ts
@@ -1,13 +1,21 @@
-import client from '../../database';
+import { Pool } from 'pg';
+import DBLoaderInstance, { DBLoader } from '../../loaders/database';
 import { User } from './interface/user.interface';
 
 class UserRepository {
+    private dbLoader: DBLoader;
+    private client: Pool;
+
+    constructor() {
+        this.dbLoader = DBLoaderInstance;
+        this.client = this.dbLoader.getClient();
+    }
+
     async getAll(): Promise<User[]> {
         try {
-            const conn = await client.connect();
-            const query = `SELECT * FROM users`;
-            const result = await conn.query(query);
-            conn.release();
+            const query = `SELECT id, username, first_name, last_name FROM users`;
+            const result = await this.client.query(query);
+
             return result.rows;
         } catch (error) {
             throw new Error(`Cannot get users ${error}`);
@@ -16,10 +24,9 @@ class UserRepository {
 
     async getUserById(id: number): Promise<User> {
         try {
-            const conn = await client.connect();
             const query = `SELECT * FROM users WHERE id='${id}'`;
-            const result = await conn.query(query);
-            conn.release();
+            const result = await this.client.query(query);
+
             return result.rows[0];
         } catch (error) {
             throw new Error(`Cannot get user with id = ${id}. Error: ${error}`);
@@ -28,34 +35,28 @@ class UserRepository {
 
     async create(user: User): Promise<User> {
         try {
-            const conn = await client.connect();
             const query = `INSERT INTO users (username, password, first_name, last_name) VALUES ($1, $2, $3, $4) RETURNING *`;
 
-            const result = await conn.query(query, [
+            const result = await this.client.query(query, [
                 user.username,
                 user.password,
                 user.first_name,
                 user.last_name
             ]);
-            conn.release();
+
             return result.rows[0];
         } catch (error) {
+            console.error(error);
             throw new Error(`Cannot create user: ${error}`);
         }
     }
 
     async findOneByUsername(username: string): Promise<User | null> {
         try {
-            const conn = await client.connect();
-
             const sql = `SELECT password FROM users WHERE username = '${username}';`;
-
-            const result = await conn.query(sql);
-
-            conn.release();
+            const result = await this.client.query(sql);
 
             if (result.rows.length) return result.rows[0];
-
             return null;
         } catch (error) {
             throw new Error(`Cannot find user with username ${username}. Error: ${error}`);
