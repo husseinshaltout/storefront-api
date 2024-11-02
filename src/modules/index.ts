751f4a68c5f5cf27beef5a9ba9da97281f0c1aeb
Hussein Shaltout <48390004+husseinshaltout@users.noreply.github.com>
2024-11-02 23:19:28 +0000

    feat(products): add products routes to app routes index

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
-import userRouter from './user/user.route';
 import Application from '../loaders/app';
+
+import userRouter from './user/user.route';
+import productRouter from './product/product.route';
 
 type appRouter = { route: string; router: express.Router };
 
-const appRouters: appRouter[] = [{ route: '/users', router: userRouter.router }];
+const appRouters: appRouter[] = [
+    { route: '/users', router: userRouter.router },
+    { route: '/products', router: productRouter.router }
+];
 
 const app = new Application();
 app.setApiRouters(appRouters);
