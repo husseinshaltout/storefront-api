78c698f6f6773d7674a399badb4f603bd37228c2
Hussein Shaltout <48390004+husseinshaltout@users.noreply.github.com>
2024-11-02 23:18:51 +0000

    refactor(auth): add auth header existence check

--- src/middleware/authTokenVerification.ts
+++ src/middleware/authTokenVerification.ts
@@ -1,12 +1,18 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 const secret = process.env.TOKEN_SECRET as string;
 // Reference : https://github.com/omargaber/ndjs-examples/blob/master/april_22/w6/src/middleware/global.ts
-const verifyAuthToken = (req: Request, res: Response, next: Function) => {
+const verifyAuthToken = (req: Request, res: Response, next: NextFunction) => {
     try {
         const authHeader = req.headers.authorization as string;
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            res.status(401);
+            res.json('Authorization header missing or malformed');
+        }
+
         const token = authHeader.split(' ')[1];
+
         const decoded = jwt.verify(token, secret);
         next();
     } catch (err) {
